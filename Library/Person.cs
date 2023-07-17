da8dad4a6a2d358422562a337aa9ac5ea43690a3
Zernez <fernando.pannullo@yahoo.it>
2023-07-17 11:03:58 +0200

    OOP Practicing ended

--- Library/Person.cs
+++ Library/Person.cs
@@ -1,7 +1,8 @@
-﻿using static System.Console;
+﻿using System.Diagnostics;
+using static System.Console;
 
 namespace Packt.Shared;
-public class Person : object
+public class Person : object, IComparable<Person>
 {
     // fields
     public string? Name;
@@ -31,5 +32,54 @@ public class Person : object
     public Person ProcreateWith(Person partner)
     {
         return Procreate(this, partner);
+    }
+
+    public static Person operator *(Person p1, Person p2) { 
+        return Person.Procreate(p1, p2); 
+    }
+
+    public static int Factorial(int number)
+    {
+        if (number < 0) { 
+            throw new ArgumentException($"{nameof(number)} cannot be less than zero."); 
+        }
+
+        return localFactorial(number);
+        
+        // local function       
+        
+        int localFactorial(int localNumber) { 
+            if (localNumber < 1) return 1; 
+            return localNumber * localFactorial(localNumber - 1); 
+        } 
+    }
+
+    // delegate field
+    public event EventHandler? Shout; // data field
+    public int AngerLevel; 
+    // method
+    public void Poke() { AngerLevel++; if (AngerLevel >= 3) { 
+            // if something is listening...
+            if (Shout != null) { 
+                // ...then call the delegate
+                Shout(this, EventArgs.Empty); 
+            } 
+        } 
+    }
+    public int CompareTo(Person? other) { 
+        if (Name is null) return 0;
+        return Name.CompareTo(other?.Name); 
+    }
+
+    // overridden methods
+    public override string ToString() { 
+        return $"{Name} is a {base.ToString()}"; 
+    }
+
+    public void TimeTravel(DateTime when) { 
+        if (when <= DateOfBirth) { 
+            throw new PersonException("If you travel back in time to a date earlier than your own birth, then the universe will explode!"); 
+        } 
+        else { WriteLine($"Welcome to {when:yyyy}!"); } 
     }
 }
---
+++ NullHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using static System.Console;
+
+int thisCannotBeNull = 4;
+// thisCannotBeNull = null; // compile error!
+int? thisCouldBeNull = null; 
+WriteLine(thisCouldBeNull); 
+WriteLine(thisCouldBeNull.GetValueOrDefault()); 
+thisCouldBeNull = 7; 
+WriteLine(thisCouldBeNull); 
+WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+Address address = new(); 
+address.Building = null; 
+address.Street = null;
+address.City = "London";
+address.Region = null;
+
+
+
+
+class Address { 
+    public string? Building; 
+    public string Street = string.Empty;
+    public string City = string.Empty;
+    public string Region = string.Empty;
+}
+
